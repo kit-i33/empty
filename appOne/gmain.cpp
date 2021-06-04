55afdd0d8d033f7e34590bbda03ae0e2869923c6
凱渡 岩下 <kaito_i0216@icloud.com>
2021-06-04 13:13:46 +0900

    Easy Graphics Coding 07

--- appOne/gmain.cpp
+++ appOne/gmain.cpp
@@ -1,8 +1,32 @@
 #include"libOne.h"
 void gmain() {
-	window(1000, 1000);
-	clear(230);
+	window(1920, 1080, full);
+	float px = 1920 / 2;
+	float py = 1080 / 2;
+	float vx = 10;
+	float radius = 150;
+	float len = radius / 1.4142f * 2;
+	float sw = radius / 8;
+	float angle = 0;
+	float angleSpeed = 0.03f;
 
-	
+	while (notQuit) {
+		px += vx;
+		angle += angleSpeed;
+		if (px < 0 || px > 1920) {
+			vx = -vx;
+			angleSpeed = -angleSpeed;
+		}
+		clear(60);
+		strokeWeight(sw);
+		stroke(255, 0, 0);
+		circle(px, py, radius * 2);
+		rectMode(CENTER);
+		rect(px, py, len, len, angle);
+		strokeWeight(sw * 4);
+		point(px, py);
+		strokeWeight(sw);
+		line(1920 / 2, 0, px, py);
+	}
 	pause();
 }
